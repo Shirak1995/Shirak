9834577cff1f92577878436139dc1d87796ec587
shirak95@mail.ru <shirak95@mail.ru>
2024-10-07 23:40:20 +0300

    Groovy

--- src/main/java/ru/vtvhw/Application.java
+++ src/main/java/ru/vtvhw/Application.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.GenericGroovyApplicationContext;
 public class Application {
     public static void main(String[] args) {
         // Spring Groovy Configuration
-        GenericGroovyApplicationContext groovyContext = new GenericGroovyApplicationContext("classpath:config/SpringConfig.groovy");
+        GenericGroovyApplicationContext groovyContext = new GenericGroovyApplicationContext("classpath:config/application.groovy");
         groovyContext.getBean("mySingletonBean");
         groovyContext.getBean("myPrototypeBean");
     }
