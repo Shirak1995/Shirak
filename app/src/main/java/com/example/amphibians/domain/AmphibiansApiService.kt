9bb71cd32889a0f852fc62a4284089fd10beccba
shirak95@mail.ru <shirak95@mail.ru>
2024-11-12 18:09:13 +0300

    ДЗ по Загрузка и отображение изображений

--- app/src/main/java/com/example/amphibians/domain/AmphibiansApiService.kt
+++ app/src/main/java/com/example/amphibians/domain/AmphibiansApiService.kt
@@ -4,6 +4,6 @@ import com.example.amphibians.data.model.Amphibian
 import retrofit2.http.GET
 
 interface AmphibiansApiService {
-    @GET ("amphibians")
+    @GET ("amphibians_notvalid_url")
     suspend fun getAmphibians(): List<Amphibian>
 }
