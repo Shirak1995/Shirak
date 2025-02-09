0c79a6f6c3aae437da7b511dbaa10c4838d07e09
shirak95@mail.ru <shirak95@mail.ru>
2025-02-09 20:02:27 +0300

    Books

--- app/src/main/java/com/example/books/network/Book.kt
+++ app/src/main/java/com/example/books/network/Book.kt
@@ -1,14 +1,6 @@
 package com.example.books.network
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
-@Serializable
-data class MarsPhoto(
-    val id: String,
-    @SerialName(value = "img_src")
-    val imgSrc: String
-)
 
 @Serializable
 data class Response(
